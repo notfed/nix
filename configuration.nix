72e3043a865b9ddd9e09714b0b0626b903b8c21f
Jay Sullivan <jaysullivan@google.com>
2021-07-31 11:15:22 -0400

    Boot EFI (STILL UNTESTED)

--- configuration.nix
+++ configuration.nix
@@ -11,17 +11,18 @@
       	./hardware-configuration.nix
     ];
 
-  # Bootloader
+  # Bootloader, https://blog.maximilianschulke.com/installing-nix-os-with-full-disk-encryption
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
-  boot.loader.grub.device = "/dev/sda"; # NOTE: DANGEROUS: WE SHOULD DYNAMIC-IZE THIS
-  boot.loader.grub.enableCryptodisk = true;
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  # Define on which hard drive you want to install Grub.
-  # boot.loader.systemd-boot.enable = true;
+  boot.loader.grub.device = "nodev"; # NOTE: DANGEROUS: WE SHOULD DYNAMIC-IZE THIS
+  boot.loader.grub.efiSupport = true;
+  #boot.loader.grub.enableCryptodisk = true;
+  #boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  #boot.loader.grub.efiInstallAsRemovable = true;
+  #boot.loader.efi.canTouchEfiVariables = false;
+  #boot.loader.systemd-boot.enable = false;
   
+  /* LUKS */
   /* ENCRYPT-PLACEHOLDER */
 
   # SSD Performance
