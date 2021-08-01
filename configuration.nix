2e1041e92e96def7c7ac5e7687ec2fc084917e09
Jay Sullivan <jaysullivan@google.com>
2021-08-01 15:13:31 -0400

    GRUB Background

--- configuration.nix
+++ configuration.nix
@@ -16,7 +16,7 @@
   boot.loader.grub.version = 2;
   boot.loader.grub.device = "nodev"; /* GRUB-DEVICE-PLACEHOLDER */
   boot.loader.grub.efiSupport = false;
-  #boot.loader.grub.splashImage = "./files/grub_background.jpg";
+  boot.loader.grub.splashImage = "./files/background.jpg";
 
   # Bootloader, EFI (https://blog.maximilianschulke.com/installing-nix-os-with-full-disk-encryption)
   /*
