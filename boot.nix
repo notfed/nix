1992c5b21c0d2c5097dc840cf1b1d297a3e4094b
Jay Sullivan <jaysullivan@google.com>
2021-08-03 14:28:44 -0400

    Split format/mount/unmount to separate scripts

--- boot.nix
+++ boot.nix
@@ -2,12 +2,12 @@
   # Bootloader, EFI
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
-  boot.loader.grub.device = "nodev"; /* GRUB-DEVICE-PLACEHOLDER */
+  boot.loader.grub.device = "nodev";
   boot.loader.grub.efiSupport = true;
   boot.loader.grub.splashImage = ./files/background.jpg;
   boot.loader.grub.efiInstallAsRemovable = true;
   boot.loader.efi.canTouchEfiVariables = false;
 
   # LUKS
-  /* ENCRYPT-PLACEHOLDER */
+  /* ENCRYPT-PLACEHOLDER */ 
 }
